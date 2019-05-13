f5e172293f6cae07b5aa948369e5a6693da6f802
Fatih Ertinaz <fertinaz@gmail.com>
2019-05-13 12:09:09 -0400

    run tests

--- run-genPrime.sh
+++ run-genPrime.sh
@@ -4,21 +4,13 @@
 # GOROOT=/usr/local/go
 # GOBIN=${GOROOT}/bin
 
-### Project directory
-# base=$HOME/dev/go
-# case=genPrime
+echo -e "Testing genPrime: " > test4.txt
 
-### Source files
-# src=genPrime.go
-
-
-echo -e "Testing genPrime: " > test.txt
-
-for bound in `seq 2 6`;
+for n in `seq 2 6`;
 do
-    echo -e $((10 ** $bound))
-    upper=$((10 ** $bound))
-    go run *.go --range=1,$upper --algorithm=1 --print=false >> test.txt
-    go run *.go --range=1,$upper --algorithm=2 --print=false >> test.txt
-    go run *.go --range=1,$upper --algorithm=3 --print=false >> test.txt
+    echo -e $((10 ** $n))
+    N=$((10 ** $n))
+    go run *.go --range=1,$N --algorithm=1 --print=false >> test4.txt
+    go run *.go --range=1,$N --algorithm=2 --print=false >> test4.txt
+    go run *.go --range=1,$N --algorithm=3 --print=false >> test4.txt
 done
