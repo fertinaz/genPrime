f49f8bf3f3380714f9dab9c49bb43e56a0369be2
Fatih Ertinaz <fertinaz@gmail.com>
2019-05-14 02:32:08 -0400

    Added go test

---
+++ sse_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestSse(t *testing.T) {
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// # of test cases = [1, 10]
+	ncases := r.Intn(10) + 1
+	fmt.Printf("SSE -- Number of test cases: %v \n", ncases)
+
+	var primes Primes
+
+	for c := 0; c < ncases; c++ {
+
+		// range = [l, u]
+		min := 30
+		max := 300
+		l := r.Intn(min)
+		u := r.Intn(max-min) + min
+		fmt.Printf("\t Range: [%v,%v] \n", l, u)
+
+		// True or false
+		// p := r.Intn(2)
+		// v := r.Intn(2)
+
+		primes = sse(l, u)
+
+		if len(primes) == 0 {
+			t.Errorf("Error!")
+		}
+
+		// if p == 1 {
+		//     printSlice(primes)
+		//	}
+
+		isValid := validate(primes, l, u)
+		if isValid {
+			fmt.Printf("\t -- Results are correct! \n")
+		} else {
+			fmt.Printf("\t -- Results are not correct! \n")
+		}
+	}
+}
